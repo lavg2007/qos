940369eecdd1635aa9157c99cc7e0b7ea0aaac96
Gabriel Lavoie <gabriel.lavoie@usherbrooke.ca>
2020-06-26 19:09:54 -0400

    Set address in first shiftreg

--- flash_programmer/flash_programmer/flash_programmer.ino
+++ flash_programmer/flash_programmer/flash_programmer.ino
@@ -1,24 +1,45 @@
-
-#define SHIFT_SER_PIN = 12
-#define SHIFT_SRCLK_PIN = 11
-#define SHIFT_DONE_PIN = 10
-baudrate = 921600
+#define SHIFT_SER_PIN 12
+#define SHIFT_SRCLK_PIN 11
+#define SHIFT_DONE_PIN 10
+#define BAUDRATE 115200
+#define READPIN 2
 
 void setup() 
 {
-  Serial.begin(92167)  ;
-  pinMode(SHIFT_SER_PIN = 12);
+    Serial.begin(BAUDRATE)  ;
+	  pinMode(SHIFT_SER_PIN, OUTPUT);
+	  pinMode(SHIFT_SRCLK_PIN, OUTPUT);
+	  pinMode(SHIFT_DONE_PIN, OUTPUT);
+	  pinMode(READPIN, INPUT);
 }
 
 void loop() 
 {
-  // put your main code here, to run repeatedly:
-  if (Serial.available());
+    for(unsigned int i = 0; i < 30000; i++)
+    {
+        set_address(i);
+        delay(100);
+        send_address_to_serial();
+    }
+    
 }
 
 void set_address(int address)
 {
-  shiftOut(SER_PIN, SRCLK_PIN, MSBFIRST, address << 8);
-  shiftOut(SER_PIN, SRCLK_PIN, MSBFIRST, address&0xFF);
-  
+    shiftOut(SHIFT_SER_PIN, SHIFT_SRCLK_PIN, MSBFIRST, address << 8);
+  	shiftOut(SHIFT_SER_PIN, SHIFT_SRCLK_PIN, MSBFIRST, address&0xFF);
+    digitalWrite(SHIFT_DONE_PIN, HIGH);
+    digitalWrite(SHIFT_DONE_PIN, LOW);
+}
+
+void send_address_to_serial()
+{
+    char bits[9];
+    for (char i = 0; i < 8; i++)
+    {
+        bits[i] = digitalRead(i + 2) + 48;
+    }
+    bits[9] = '\n';
+    Serial.write(bits, 9);
+    Serial.print('\n');
 }
